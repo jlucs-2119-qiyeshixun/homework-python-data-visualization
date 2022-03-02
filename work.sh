7129f1f83cd1faabed2ddb22e308d9f66db12c27
waynelee-lwc <1418551940@qq.com>
2022-03-03 01:09:27 +0800

    [data]2022年 03月 03日 星期四 01:09:27 CST

---
+++ work.sh
@@ -0,0 +1,11 @@
+python3 script/popular_data.py 
+
+tme=`date`
+commit="git commit -m'[data]`date`'"
+echo $commit
+git add .
+
+echo $commit | bash
+
+git branch
+git push origin master:popular_data
