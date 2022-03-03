db571244bbee0bab801043ee08cdd45c189e5427
liweicong.lwc <liweicong.lwc@bytedance.com>
2022-03-03 17:37:08 +0800

    [feat]新增了对tid数量及占比的处理

--- script/popular_data.py
+++ script/popular_data.py
@@ -14,7 +14,7 @@ cwd = os.getcwd()
 
 url = 'https://api.bilibili.com/x/web-interface/popular'
 params = {
-    'ps' : 50,
+    'ps' : 20,
     'pn' : 1
 }
 headers = {
@@ -38,6 +38,25 @@ while True:
     else:
         break
 
-#print(json.dumps(tot_li,ensure_ascii=False))
+print(json.dumps(tot_li,ensure_ascii=False))
 f = open(cwd + f'/data/popular/{getTime()}.json','w',encoding='utf-8')
 json.dump(tot_li,f,ensure_ascii=False)
+# title_li = [item['title'] for item in tot_li]
+# rank_li = [item['stat']['his_rank'] for item in tot_li]
+# rcmd_reason = [item['rcmd_reason']['content'] for item in tot_li]
+# tid_mp = {}
+
+# for item in tot_li:
+#     if item['tid'] in tid_mp:
+#         tid_mp[item['tid']] += 1
+#     else:
+#         tid_mp[item['tid']] = 1
+
+# for i in range(len(title_li)):
+#     print(i,title_li[i],rank_li[i],rcmd_reason[i])
+
+# for k,v in tot_li[0].items():
+#     print(k,v)
+
+# for k,v in tid_mp.items():
+#     print(k,v)
