91bf4322a55e463af3b070d8c92a9968f00935cf
liweicong.lwc <liweicong.lwc@bytedance.com>
2022-03-04 01:15:48 +0800

    [feat]增加了热榜数据的可视化

--- script/popular_visualization.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-# 数据准备
-x = [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
-y = [5, 3, 6, 20, 17, 16, 19, 30, 32, 35]
-# 使用Matplotlib画折线图
-plt.plot(x, y)
-plt.show()
